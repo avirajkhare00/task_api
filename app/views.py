e832e697826d1a4725513e940965dfdb7135483e
avirajkhare00 <avirajkhare00@gmail.com>
2025-04-06 18:20:12 +0530

    [Aviraj] Added signup api

--- app/views.py
+++ app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from .serializers import UserSerializer
 
-# Create your views here.
+User = get_user_model()
+
+class SignupAPIView(generics.CreateAPIView):
+    """
+    API view for user registration.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
--- task_api/urls.py
+++ task_api/urls.py
@@ -4,9 +4,11 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from app.views import SignupAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/signup/', SignupAPIView.as_view(), name='signup'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
