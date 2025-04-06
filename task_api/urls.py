949de4f82a3c7b4e01e060e48b63510e0b821f69
avirajkhare00 <avirajkhare00@gmail.com>
2025-04-06 21:30:31 +0530

    [Aviraj] Added login/ url

--- task_api/urls.py
+++ task_api/urls.py
@@ -9,6 +9,7 @@ from app.views import SignupAPIView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/signup/', SignupAPIView.as_view(), name='signup'),
+    path('api/login/', TokenObtainPairView.as_view(), name='login'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
